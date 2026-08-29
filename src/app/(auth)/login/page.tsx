2590708de45dda724624d9cfe98b07c140b863ba
TusharMamun <tusharmamun345@gmail.com>
2026-08-30 00:45:39 +0600

    inital deply successful

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const logingPage = () => {
+  return (
+    <div>
+        <h1>hellow same alldata </h1>
+<Button>Save Data</Button>  </div>
+  )
+}
+
+export default logingPage
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const homePage = () => {
+  return (
+    <div>Home page </div>
+  )
+}
+
+export default homePage
